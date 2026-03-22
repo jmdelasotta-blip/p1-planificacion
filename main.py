c67b3ebd1d0ad3d20ebea62d010408e9fe5fdc44
jose-antonio-goycoolea-26 <jagoycoolea@miuandes.cl>
2026-03-22 17:32:26 -0300

    avance con la solución del problema

--- main.py
+++ main.py
@@ -17,17 +17,48 @@ with open("tareas.txt", "r") as i:
     tareas = []
     for line in i:
         id_tarea, duracion, categoria = line.strip().split(",")
-        tareas.append(Tarea(id_tarea, int(duracion), categoria))
+        tareas.append((id_tarea, int(duracion), categoria))
 
 with open("recursos.txt", "r") as j:
     recursos = []
     for line in j:
         id_recursos, categoria= line.strip().split(",")
-        recursos.append(Recurso(id_recursos, categoria))
-print(f"Estas son las tareas a asignar: {tareas}")
-print(f"Estas son las tareas a asignar: {recursos}")
-for m in recursos:
-    print(m)
-print(recursos[0])
-# Pon esto al final de tu archivo main.py
-print("¡Lectura terminada!")
+        recursos.append((id_recursos, categoria))
+
+for i in range(len(recursos)):
+    print(recursos[i])
+print(" ")
+for i in range(len(tareas)):
+    print(tareas[i])
+tkstime=0
+for i in range(len(tareas)):
+    tkstime=tkstime+tareas[i][1]
+print(tkstime)
+
+r1queue=[]
+r2queue=[]
+r3queue=[]
+def r1process():
+    r1counter=0
+    #Recurso 1 admite tareas 'CAT_A'
+    for i in range(len(tareas)):
+        if tareas[i][2]=='CAT_A':
+            r1counter=r1counter+tareas[i][1]
+            r1queue.append(tareas[i][0])
+            tareas.remove(tareas[i])
+            print(r1counter)
+        break
+
+r1process()
+print(tareas)
+def r2process():
+    pass
+def r3process():
+    pass
+
+
+
+
+with open("mis_resultados.txt", "w") as archivo_nuevo:
+    archivo_nuevo.write("Hola, este es mi primer texto guardado.\n")
+    archivo_nuevo.write("Esta es la segunda línea.")
